e9059b77f4b3228fb473672524602d7964cb25cc
jonghwa hong <zzzonghwa@gmail.com>
2022-06-27 17:31:24 +0900

    post data

--- webapp/controller/EditPost.controller.js
+++ webapp/controller/EditPost.controller.js
@@ -7,11 +7,34 @@ sap.ui.define([
 
 	return Controller.extend("board.controller.EditPost", {
         onInit() {
+            this.oModel = this.getOwnerComponent().getModel();
+
         },
 
         onNavBack: function (oEvt) {
             console.log(oEvt);
             this.getOwnerComponent().getRouter().navTo('Board');
+        },
+
+        post: function (oEvt) {
+          const oData = {
+            id: Number(this.byId('id').getValue()),
+            title: this.byId('title').getValue(),
+            content: this.byId('content').getValue()
+        };
+          console.log(oData);
+          this.oModel.create("/BoardHong", oData, {
+              async: true,
+            success: function (oData, response) {
+              console.log(oData);
+              console.log(response);
+            },
+            error: function (response) {
+              window.alert("sorry");
+            },
+          });
+
         }
+
 	});
 });
